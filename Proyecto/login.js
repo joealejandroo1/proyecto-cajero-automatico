5fd8a8c1c5376e85a40bfa4b893078b4d35be9ea
Ruben <ruben.drg04@gmail.com>
2025-03-22 16:42:08 -0400

    codigo de login terminado

--- Proyecto/login.js
+++ Proyecto/login.js
@@ -1,32 +1,32 @@
 const usuarios = [
-    {
-      usuario: "Ruben",
-      clave: "contraseña123"
-    },
-    {
-      usuario: "Daniela",
-      clave: "password2023"
-    },
-    {
-      usuario: "Joe",
+  {
+    usuario: "Ruben",
+    clave: "contraseña123"
+  },
+  {
+    usuario: "Daniela",
+    clave: "password2023"
+  },
+  {
+    usuario: "Joe",
+    clave: "guest"
+  },
+  {
+      usuario: "Carlos",
       clave: "guest"
-    }
-    {
-        usuario: "Carlos",
-        clave: "guest"
-      }
-  ];
+  }
+];
 
 document.getElementById("boton").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const usuario = document.getElementById("usuario").value;
-    const clave = document.getElementById("clave").value;
-    const usuarioEncontrado = usuarios.find(usuarios => usuarios.usuario === usuario && usuarios.clave === clave;
-    
-    if (usuarioEncontrado) {
-      alert("Bienvenido " + usuarioEncontrado.usuario);
-    } else {
-      alert("Usuario no encontrado");
-    }
-
-
+  event.preventDefault();
+  const usuario = document.getElementById("usuario").value;
+  const clave = document.getElementById("clave").value;
+  const usuarioEncontrado = usuarios.find((usuario) => usuario.usuario === usuario && usuario.clave === clave);
+  
+  if (usuarioEncontrado) {
+    alert("Bienvenido " + usuarioEncontrado.usuario);
+    window.location.href = "index.html";
+  } else {
+    alert("Usuario no encontrado");
+  }
+});
